8dace9fa733080d37b8d647d2231fc9f3004a675
19jaswanth <43063971+19jaswanth@users.noreply.github.com>
2020-06-02 22:49:22 +0530

    setup.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy
+
+class LinearRegression(object):
+  """
+  An implementation of linear regression model
+  """
+  
+  def fit(_input, _output):
+    pass
+    
+  def predict(_input):
+    pass
+    
+    
